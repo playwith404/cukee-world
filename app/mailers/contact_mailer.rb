a52467a65aa144bd6c83e9d593f581409aece4c5
이지우 <dimsum44@naver.com>
2026-01-09 16:14:14 +0900

    chore: 환경변수 처리 및 배포 설정

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ApplicationMailer
-  default from: "noreply@cukee.cloud"
+  default from: ENV.fetch("MAILER_FROM_EMAIL", "noreply@cukee.cloud")
 
   def new_contact(contact)
     @contact = contact
 
     mail(
-      to: "playwith404@gmail.com",
+      to: ENV.fetch("CONTACT_EMAIL", "playwith404@gmail.com"),
       subject: "[playwith404 문의] #{contact.inquiry_type_label} - #{contact.name}"
     )
   end
